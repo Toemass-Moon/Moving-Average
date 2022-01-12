6298fe65a6ee62560572aad17cda8a95d89dfffa
Toemass-Moon <thomaz2moon@gmail.com>
2022-01-11 16:17:37 -0800

    added in the requirements and got rid of numpy as i ended up not using it anymore

--- browser_app.py
+++ browser_app.py
@@ -3,7 +3,6 @@ import pandas_datareader as pdr
 from datetime import datetime
 import streamlit as st
 import pandas as pd
-import numpy as np
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
